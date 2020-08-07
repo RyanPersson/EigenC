cb82fcfcb09754b3f1a41cf78c58a2802568c5d2
Ryan <perssonryan@gmail.com>
2020-08-06 18:20:35 -0700

    Added goog examples to test_matrix.cpp

--- test_matrix.cpp
+++ test_matrix.cpp
@@ -8,19 +8,21 @@
 
 int main() {
 
+    Identity_Matrix i_7x7(7); //generates a 7x7 identity matrix
+    i_7x7.print_matrix();
 
-    Identity_Matrix i_5(5);
-    i_5.print_matrix();
+    Matrix a_2x3(2,3); //New 2x3 matrix
+    a_2x3.fill(5);
+    a_2x3.print_matrix();
 
-    Matrix new_5x3(5,3);
-    new_5x3.fill(15);
-    new_5x3.print_matrix();
+    Matrix b_3x7(3,7); //New 3x7 matrix
+    b_3x7.fill(2);
+    b_3x7.print_matrix();
 
-    Matrix new_test_left = multiply(i_5, new_5x3);
-    new_test_left.print_matrix();
+    Matrix ab_2x7 = multiply(a_2x3, b_3x7);
+    ab_2x7.print_matrix();
 
-    Matrix new_test_right = multiply(new_5x3, i_5);
-    new_test_right.print_matrix();
+    multiply(ab_2x7,i_7x7).print_matrix();
 
     return 0;
 }
